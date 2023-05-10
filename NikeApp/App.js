9f88f2c25a9f364bf3f20aaa5121ae2b93d0836f
elgun <elgun.abasquliyev2@gmail.com>
2023-05-10 23:12:50 +0400

    Add ProductDetailsScreen

--- NikeApp/App.js
+++ NikeApp/App.js
@@ -3,11 +3,13 @@ import { StyleSheet, View } from "react-native";
 
 // Screens
 import ProductsScreen from "./src/screens/ProductsScreen";
+import ProductDetailsScreen from "./src/screens/ProductDetailsScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ProductsScreen />
+      {/* <ProductsScreen /> */}
+      <ProductDetailsScreen />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,7 +19,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
---
+++ NikeApp/src/screens/ProductDetailsScreen.js
@@ -0,0 +1,103 @@
+import {
+  Image,
+  StyleSheet,
+  ScrollView,
+  View,
+  FlatList,
+  Text,
+  Pressable,
+  useWindowDimensions,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import products from "../data/products";
+
+const ProductDetailsScreen = () => {
+  const product = products[0];
+  const { width } = useWindowDimensions();
+
+  const addToCartHandler = () => {
+    console.warn("Add to cart");
+  };
+  return (
+    <View>
+      {/* Image Carousel */}
+      <ScrollView>
+        <FlatList
+          data={product.images}
+          renderItem={({ item }) => (
+            <Image
+              source={{ uri: item }}
+              style={{
+                width: width,
+                aspectRatio: 1,
+              }}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+        />
+        {/* Title */}
+        <View style={{ padding: 20 }}>
+          <Text style={styles.title}>{product.name}</Text>
+
+          {/* Price */}
+          <Text style={styles.price}>${product.price}</Text>
+
+          {/* Description */}
+          <Text style={styles.description}>{product.description}</Text>
+        </View>
+      </ScrollView>
+
+      {/* Add to cart button */}
+      <Pressable onPress={addToCartHandler} style={styles.cartButton}>
+        <Ionicons
+          name="cart"
+          size={24}
+          color="white"
+          style={{ marginRight: 10 }}
+        />
+        <Text style={styles.cartButtonText}>Add to cart</Text>
+      </Pressable>
+
+      {/* Navigation icon */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 34,
+    fontWeight: "500",
+    marginVertical: 10,
+  },
+  price: {
+    fontWeight: "500",
+    fontSize: 16,
+    letterSpacing: 1.5,
+  },
+  description: {
+    marginVertical: 10,
+    fontSize: 18,
+    lineHeight: 30,
+    fontWeight: "300",
+  },
+  cartButton: {
+    position: "absolute",
+    backgroundColor: "black",
+    bottom: 30,
+    width: "90%",
+    alignSelf: "center",
+    padding: 20,
+    borderRadius: 100,
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  cartButtonText: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 16,
+  },
+});
+
+export default ProductDetailsScreen;
