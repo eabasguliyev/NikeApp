aded9ce5e9eca0d7d5f7456de61c47ac40d67097
elgun <elgun.abasquliyev2@gmail.com>
2023-05-10 22:31:54 +0400

    Add ProducsScreen

---
+++ NikeApp/src/screens/ProductsScreen.js
@@ -0,0 +1,31 @@
+import { StyleSheet, FlatList, View, Image } from "react-native";
+import products from "../data/products";
+
+const ProductsScreen = () => {
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => (
+        <View style={styles.itemContainer}>
+          <Image
+            source={{
+              uri: item.image,
+            }}
+            style={styles.image}
+          />
+        </View>
+      )}
+      numColumns={2}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    width: "50%",
+    padding: 1,
+  },
+  image: { width: "100%", aspectRatio: 1 / 1 },
+});
+
+export default ProductsScreen;
